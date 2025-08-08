ed4afc15e63f62c3dcd9805f81a9df34c4990cd7
Eisenwall <Eisenwall@proton.me>
2025-08-08 13:33:12 +0000

    BT

--- binary_trees.h
+++ binary_trees.h
@@ -11,12 +11,4 @@ struct binary_tree_s
     struct binary_tree_s *left;
     struct binary_tree_s *right;
 };
-
-typedef struct binary_tree_s binary_tree_t;
-
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
-size_t binary_tree_depth(const binary_tree_t *tree);
-void binary_tree_print(const binary_tree_t *tree);
-
 #endif
