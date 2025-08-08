12f665338bc4814c694281cd3a29beab0ef644a3
Eisenwall <Eisenwall@proton.me>
2025-08-08 14:06:17 +0000

    BT

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -26,5 +26,5 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	return ((int)_height(tree->right) - (int)_height(tree->left));
+	return ((int)_height(tree->left) - (int)_height(tree->right));
 }
