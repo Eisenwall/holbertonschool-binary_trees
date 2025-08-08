3ca2863ca2ddc171f55e62d48c9df3106d914047
Eisenwall <Eisenwall@proton.me>
2025-08-08 13:37:43 +0000

    BT

--- binary_trees.h
+++ binary_trees.h
@@ -2,19 +2,18 @@
 #define BINARY_TREES_H
 
 #include <stddef.h>
-#include <stdlib.h>
-#include <stdio.h>
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
-};
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+}
+
 typedef struct binary_tree_s binary_tree_t;
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
-size_t binary_tree_leaves(const binary_tree_t *tree);
-size_t binary_tree_depth(const binary_tree_t *tree);
 void binary_tree_print(const binary_tree_t *tree);
+size_t binary_tree_size(const binary_tree_t *tree);
+size_t binary_tree_depth(const binary_tree_t *tree);
 #endif
